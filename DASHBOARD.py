dd8a5fb6b875fca822478b5129e9cd7f72552f79
yazzledazzle <yhana.lucas@gmail.com>
2024-01-31 11:52:52 +0800

    Census update

--- DASHBOARD.py
+++ DASHBOARD.py
@@ -74,6 +74,9 @@ def home():
     elif goto == 'Upload data or external content':
         upload_data()
         show_update_log()
+    elif goto == 'Census':
+        census_data = st.sidebar.selectbox('Select dataset', ['Total by state', 'Geographic breakdown', 'Aboriginal and Torres Strait Islander status', 'Sex and Age'])
+        census(census_data)
     return
 
 def data_updates():
@@ -3184,6 +3187,152 @@ def import_waitlist_manual(manualdata):
     Waitlist_clear_new_data(newWaitlistData)
     update_log(max_date, update_date, 'Waitlist trend - statewide')
     return
+
+def census(census_data):
+    if census_data == 'Total by state':
+        df = pd.read_csv(ROGSHomelessnessdf, encoding='latin-1')
+        df = df.sort_values(by='Year', ascending=True)
+        df['Year'] = df['Year'].astype(str)
+        df = df.rename(columns={'Aust': 'National'})
+        regions = ['National', 'WA', 'Vic', 'Qld', 'SA', 'NSW', 'Tas', 'NT', 'ACT']
+
+        df = df[df['Measure']=="Homelessness; by homelessness operational group"]
+
+        df['Description2'] = df['Description2'].fillna('Persons')
+        col1, col2, col3, col4 = st.columns(4)
+        with col1:
+            Desc2 = st.selectbox('Select Description2 filter', df['Description2'].unique(), index=0)
+            df = df[df['Description2'] == Desc2]
+        with col2:
+            if len(df['Description3'].unique()) > 1:
+                Desc3 = st.selectbox('Select Description3 filter', df['Description3'].unique(), index=0)
+                df = df[df['Description3'] == Desc3]
+        with col3:
+            if len(df['Description4'].unique()) > 1:
+                Desc4 = st.selectbox('Select Description4 filter', df['Description4'].unique(), index=0)
+                df = df[df['Description4'] == Desc4]
+        with col4:
+            datalabels = st.radio('Data labels', ['On', 'Off'], index=0, horizontal=True, key='censustotaldatalabels')
+
+        df = df.sort_values(by=['Year'], ascending=True)
+        #for region in regions, filter df for region, plotly bar, x=Year, y=Value, color=Region, group
+        fig = go.Figure()
+        yunits = df['Unit'].unique()[0]
+        for region in regions:
+            fig.add_trace(go.Bar(x=df['Year'], y=df[region], name=region))
+        fig.update_layout(barmode='group', title='Homelessness; by homelessness operational group', xaxis_title="Year", yaxis_title=yunits)
+        if datalabels == 'On':
+            fig.update_traces(texttemplate='%{text:.2s}', textposition='inside', text=df[region])
+        st.plotly_chart(fig, use_container_width=True)
+
+    elif census_data == 'Geographic breakdown':
+        st.markdown(f'Note - error in ABS table builder for LGAs, awaiting help on file and limited to SA4 for time being however quick swap in once received')
+        df = pd.read_csv('DATA/PROCESSED DATA/Census/Multiyear/SA4Income_1621.csv')
+        df = df.melt(id_vars=['OPGP HOMELESSNESS OPERATIONAL GROUPS', 'SA4', 'CENSUS_YEAR'], var_name='Income', value_name='Value')
+        #replace Not Applicable with Not applicable
+        df['OPGP HOMELESSNESS OPERATIONAL GROUPS'] = df['OPGP HOMELESSNESS OPERATIONAL GROUPS'].replace('Not Applicable', 'Not applicable')
+        df = df[df['Income']=='TOTAL']
+        df = df.drop(columns=['Income'])
+        #in SA4, replace "Western Australia - " with ""
+        df['SA4'] = df['SA4'].str.replace('Western Australia - ', '')
+        #drop rows where Value = 0
+        df = df[df['Value']!=0]
+        col1, col2, COL3 = st.columns(3)
+        with col1:
+            geoselect = st.multiselect('Select area', df['SA4'].unique(), default = ['Perth - South East', 'Perth - North West', 'Perth - North East', 'Perth - South West', 'Perth - Inner'])
+        with col2:
+            datalabels = st.radio('Data labels', ['On', 'Off'], index=0, horizontal=True, key='censusSA4datalabels')
+        dftotal = df[df['OPGP HOMELESSNESS OPERATIONAL GROUPS']=='Total']
+        dfna = df[df['OPGP HOMELESSNESS OPERATIONAL GROUPS']=='Not applicable']
+        dftotalna = pd.concat([dftotal, dfna])
+        #pivot so OPGP HOMELESSNESS OPERATIONAL GROUPS is columns
+        dftotalna = dftotalna.pivot(index=['SA4', 'CENSUS_YEAR'], columns='OPGP HOMELESSNESS OPERATIONAL GROUPS', values='Value').reset_index()
+        dftotalna['Total homelessness'] = dftotalna['Total'] - dftotalna['Not applicable']
+        dftotalna['per 10k'] = dftotalna['Total homelessness'] / dftotalna['Total'] * 10000
+        #drop Not applicable
+        dftotalna = dftotalna.drop(columns=['Not applicable'])
+        #melt back to long format
+        dftotalna = dftotalna.melt(id_vars=['SA4', 'CENSUS_YEAR'], var_name='Measure', value_name='Value')
+        df = df[df['OPGP HOMELESSNESS OPERATIONAL GROUPS']!='Total']
+        df = df[df['OPGP HOMELESSNESS OPERATIONAL GROUPS']!='Not applicable']
+        df16 = df[df['CENSUS_YEAR']==2016]
+        df21 = df[df['CENSUS_YEAR']==2021]
+
+        #filter dftotal21 for geoselect
+        df10k = dftotalna[dftotalna['SA4'].isin(geoselect)]
+        df10k = df10k[df10k['Measure']=='per 10k']
+        df10k = df10k.drop(columns=['Measure'])
+
+        dftotal = dftotalna[dftotalna['SA4'].isin(geoselect)]
+        dftotal = dftotal[dftotal['Measure']=='Total homelessness']
+        dftotal = dftotal.drop(columns=['Measure'])
+
+        dftotal16 = dftotal[dftotal['CENSUS_YEAR']==2016]
+        dftotal21 = dftotal[dftotal['CENSUS_YEAR']==2021]
+
+        fig = go.Figure()
+        df10k16 = df10k[df10k['CENSUS_YEAR']==2016]
+        df10k21 = df10k[df10k['CENSUS_YEAR']==2021]
+        
+        
+        
+        if datalabels == 'On':
+            fig.add_trace(go.Bar(x=df10k21['SA4'], y=df10k21['Value'], name='2021', text=df10k21['Value'], textposition='inside', texttemplate='%{text:.2s}'))
+            fig.add_trace(go.Bar(x=df10k16['SA4'], y=df10k16['Value'], name='2016', text=df10k16['Value'], textposition='inside', texttemplate='%{text:.2s}'))
+        else:
+            fig.add_trace(go.Bar(x=df10k21['SA4'], y=df10k21['Value'], name='2021'))
+            fig.add_trace(go.Bar(x=df10k16['SA4'], y=df10k16['Value'], name='2016'))
+        
+        fig.update_layout(barmode='group', title='Homelessness; per 10k population', yaxis_title='Value', xaxis_tickangle=-45, height=800, width=3200/len(geoselect))
+        st.plotly_chart(fig)
+
+        fig = go.Figure()
+        if datalabels == 'On':
+            fig.add_trace(go.Bar(x=dftotal21['SA4'], y=dftotal21['Value'], name='2021', text=dftotal21['Value'], textposition='inside', texttemplate='%{text:.2s}'))
+            fig.add_trace(go.Bar(x=dftotal16['SA4'], y=dftotal16['Value'], name='2016', text=dftotal16['Value'], textposition='inside', texttemplate='%{text:.2s}'))
+        else:
+            fig.add_trace(go.Bar(x=dftotal16['SA4'], y=dftotal16['Value'], name='2016'))
+            fig.add_trace(go.Bar(x=dftotal21['SA4'], y=dftotal21['Value'], name='2021'))
+
+        fig.update_layout(barmode='group', title='Homelessness; total', yaxis_title='Value', xaxis_tickangle=-45, height=800, width=3200/len(geoselect))
+        st.plotly_chart(fig)
+
+        for sa4 in geoselect:
+            filtereddf16 = df16[df16['SA4']==sa4]
+            filtereddf21 = df21[df21['SA4']==sa4]
+        
+            #plotly bar, x=OPGP HOMELESSNESS OPERATIONAL GROUPS, y=Value, color=CENSUS_YEAR
+            fig = go.Figure()
+            fig.add_trace(go.Bar(x=filtereddf16['OPGP HOMELESSNESS OPERATIONAL GROUPS'], y=filtereddf16['Value'], name='2016'))
+            if datalabels == 'On':
+                fig.update_traces(texttemplate='%{text:.2s}', textposition='inside', text=filtereddf16['Value'])
+            fig.add_trace(go.Bar(x=filtereddf21['OPGP HOMELESSNESS OPERATIONAL GROUPS'], y=filtereddf21['Value'], name='2021'))
+            if datalabels == 'On':
+                fig.update_traces(texttemplate='%{text:.2s}', textposition='inside', text=filtereddf21['Value'])
+            fig.update_layout(barmode='group', title=f'Homelessness groups - {sa4}', yaxis_title='persons', height=800)
+            st.plotly_chart(fig, use_container_width=True)
+
+            #pie chart for each year
+            col1, col2 = st.columns(2)
+            
+            fig = go.Figure()
+            fig.add_trace(go.Pie(labels=filtereddf16['OPGP HOMELESSNESS OPERATIONAL GROUPS'], values=filtereddf16['Value'], name='2016'))
+            fig.update_layout(title='Homelessness groups - 2016', yaxis_title='persons')
+            if datalabels == 'On':
+                fig.update_traces(textposition='inside', textinfo='percent+value')
+            with col1:
+                st.plotly_chart(fig)
+            fig = go.Figure()
+            fig.add_trace(go.Pie(labels=filtereddf21['OPGP HOMELESSNESS OPERATIONAL GROUPS'], values=filtereddf21['Value'], name='2021'))
+            fig.update_layout(title='Homelessness groups - 2021', xaxis_title="Homelessness operational group", yaxis_title='Value')
+            if datalabels == 'On':
+                fig.update_traces(textposition='inside', textinfo='percent+value')
+            with col2:    
+                st.plotly_chart(fig)
+    return
+
+
+
 
 
 if __name__ == "__main__":
